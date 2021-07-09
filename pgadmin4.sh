142b0e76869534c39ec626b6541c0aa50d16748c
nandanchitale <nandanchitale@gmail.com>
2021-07-09 19:30:48 +0530

    Minor change in bash script

--- pgadmin4.sh
+++ pgadmin4.sh
@@ -1,14 +1,13 @@
 #! /bin/sh
 
-echo "\n\n Entering PGAdmin installation Directory "
-echo " Installation Dir : /run/media/nandan/G/Softwares/linux/pgadmin/ "
-
-echo "\n\n cd /run/media/nandan/G/Softwares/linux/pgadmin/"
+str=$'\n\n Entering PGAdmin installation Directory\n Installation Dir : /run/media/nandan/G/Softwares/linux/pgadmin/ \n cd /run/media/nandan/G/Softwares/linux/pgadmin/'
+echo "$str"
 cd /run/media/nandan/G/Softwares/linux/pgadmin/
 
-echo "\n\n Activating Virtual Environment... "
-echo " source env/bin/activate "
+str=$'\n\n Activating Virtual Environment... \n\n source env/bin/activate '
+echo "$str"
 source env/bin/activate
 
-echo "\n\n Starting pgadmin4 with root previliges ... "
+str=$'\n\n Starting pgadmin4 with root previliges ... "'
+echo "$str"
 sudo pgadmin4
